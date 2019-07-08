a293a5e5dfc9d86ed22f44ab8db7f3c60ed51109
szilard-molnar <mszilard86@gmail.com>
2019-07-08 18:06:14 -0400

    fixed counting, final version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,170 +1,148 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-var randomNumber;
-var addingUpNumber;
-var crystal1;
-var crystal2;
-var crystal3;
-var crystal4;
-var winning;
-var losing;
-var wins = 0;
-var losses = 0;
+    var randomNumber;
+    var addingUpNumber;
+    var crystal1;
+    var crystal2;
+    var crystal3;
+    var crystal4;
+    var winning;
+    var losing;
+    var wins = 0;
+    var losses = 0;
 
-$("#startButton").click(function()
-{
-      
-    randomNumber = Math.floor(Math.random() * (120-19+1)) + 19;
+    $("#startButton").click(function () {
+
+        randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
         $("#randomNumber").html("Your number to reach is: " + randomNumber);
-    
-    addingUpNumber = 0;
-    
-    crystal1 = Math.floor(Math.random() * (12-1+1)) + 1;
-    console.log(crystal1);
-    crystal2 = Math.floor(Math.random() * (12-1+1)) + 1;
-    console.log(crystal2);
-    crystal3 = Math.floor(Math.random() * (12-1+1)) + 1;
-    console.log(crystal3);
-    crystal4 = Math.floor(Math.random() * (12-1+1)) + 1;
-    console.log(crystal4);
-    
-    winning = false;
-    losing = false;
 
-    numberSum();
+        addingUpNumber = 0;
 
-    $("#startButton").html("<button id='restartButton'>Restart!</button>");
+        crystal1 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        console.log(crystal1);
+        crystal2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        console.log(crystal2);
+        crystal3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        console.log(crystal3);
+        crystal4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        console.log(crystal4);
 
-    $("#myScore").html("<p>Your current score is: " + 0 + "</p>");
-    $("#wins").html("<p>Wins: " + wins + "</p>");
-    $("#losses").html("<p>Losses: " + losses + "</p>");
-    $("#message").empty();
-    
-function numberSum()
-    {
-        $("#crystal_1").click(function()
-            {
-            if(winning === true || losing === true)
-            {
+        winning = false;
+        losing = false;
+
+        $("#startButton").html("<button id='restartButton'>Restart!</button>");
+
+        $("#myScore").html("<p>Your current score is: " + 0 + "</p>");
+        $("#wins").html("<p>Wins: " + wins + "</p>");
+        $("#losses").html("<p>Losses: " + losses + "</p>");
+        $("#message").empty();
+
+    });
+
+        $("#crystal_1").click(function () {
+            if (winning === true || losing === true) {
                 return;
             }
             addingUpNumber = addingUpNumber + crystal1;
             $("#myScore").html("<p>Your current score is: " + addingUpNumber + "</p>");
-                if(randomNumber === addingUpNumber)
-                {
-                    winning = true;
-                    $("#message").append("<p>You Win!</p>");
-                    wins++;
-                    $("#wins").html("Wins: " + wins);
-                }
-                else if(randomNumber < addingUpNumber)
-                {
-                    losing = true;   
-                    $("#message").append("<p>You Lost...</p>");
-                    losses++;
-                    $("#losses").html("Losses: " + losses);
-                }
-            }) 
-    
-    
-        $("#crystal_2").click(function()
-            {
-            if(winning === true || losing === true)
-            {
-            return;
+            console.log(crystal1);
+            if (randomNumber === addingUpNumber) {
+                winning = true;
+                $("#message").append("<p>You Win!</p>");
+                wins++;
+                $("#wins").html("Wins: " + wins);
+            }
+            else if (randomNumber < addingUpNumber) {
+                losing = true;
+                $("#message").append("<p>You Lost...</p>");
+                losses++;
+                $("#losses").html("Losses: " + losses);
+            }
+        })
+
+
+        $("#crystal_2").click(function () {
+            if (winning === true || losing === true) {
+                return;
             }
             addingUpNumber = addingUpNumber + crystal2;
             $("#myScore").html("<p>Your current score is: " + addingUpNumber + "</p>");
-                if(randomNumber === addingUpNumber)
-                {
-                    winning = true;
-                    $("#message").append("<p>You Win!</p>");
-                    wins++;
-                    $("#wins").html("Wins: " + wins);
-                
-                }
-                else if(randomNumber < addingUpNumber)
-                {
-                    losing = true;
-                    $("#message").append("<p>You Lost...</p>");
-                    losses++;
-                    $("#losses").html("Losses: " + losses);
-                
-                }
-            })
-    
-        $("#crystal_3").click(function()
-            {
-            if(winning === true || losing === true)
-            {
+            if (randomNumber === addingUpNumber) {
+                winning = true;
+                $("#message").append("<p>You Win!</p>");
+                wins++;
+                $("#wins").html("Wins: " + wins);
+
+            }
+            else if (randomNumber < addingUpNumber) {
+                losing = true;
+                $("#message").append("<p>You Lost...</p>");
+                losses++;
+                $("#losses").html("Losses: " + losses);
+
+            }
+        })
+
+        $("#crystal_3").click(function () {
+            if (winning === true || losing === true) {
                 return;
             }
             addingUpNumber = addingUpNumber + crystal3;
             $("#myScore").html("<p>Your current score is: " + addingUpNumber + "</p>");
-                if(randomNumber === addingUpNumber)
-                {
-                    winning = true;
-                    $("#message").append("<p>You Win!</p>");
-                    wins++;
-                    $("#wins").html("Wins: " + wins);
-            
-                }
-                else if(randomNumber < addingUpNumber)
-                {
-                    losing = true;
-                    $("#message").append("<p>You Lost...</p>");
-                    losses++;
-                    $("#losses").html("Losses: " + losses);
-                }
-            })
+            if (randomNumber === addingUpNumber) {
+                winning = true;
+                $("#message").append("<p>You Win!</p>");
+                wins++;
+                $("#wins").html("Wins: " + wins);
 
-        $("#crystal_4").click(function()
-            {
-            if(winning === true || losing === true)
-            {
+            }
+            else if (randomNumber < addingUpNumber) {
+                losing = true;
+                $("#message").append("<p>You Lost...</p>");
+                losses++;
+                $("#losses").html("Losses: " + losses);
+            }
+        })
+
+        $("#crystal_4").click(function () {
+            if (winning === true || losing === true) {
                 return;
             }
             addingUpNumber = addingUpNumber + crystal4;
             $("#myScore").html("<p>Your current score is: " + addingUpNumber + "</p>");
-                if(randomNumber === addingUpNumber)
-                {
-                    winning = true;
-                    $("#message").append("<p>You Win!</p>");
-                    wins++;
-                    $("#wins").html("Wins: " + wins);
-    
-                }
-                else if(randomNumber < addingUpNumber)
-                {
-                    losing = true;
-                    $("#message").append("<p>You Lost...</p>");
-                    losses++;
-                    $("#losses").html("Losses: " + losses);
-            
-                }
-            })
+            if (randomNumber === addingUpNumber) {
+                winning = true;
+                $("#message").append("<p>You Win!</p>");
+                wins++;
+                $("#wins").html("Wins: " + wins);
 
-            $("#restartButton").click(function()
-            {
-                randomNumber = Math.floor(Math.random() * (120-19+1)) + 19;
-                addingUpNumber = 0;
-                crystal1 = Math.floor(Math.random() * (12-1+1)) + 1;
-                console.log(crystal1);
-                crystal2 = Math.floor(Math.random() * (12-1+1)) + 1;
-                console.log(crystal2);
-                crystal3 = Math.floor(Math.random() * (12-1+1)) + 1;
-                console.log(crystal3);
-                crystal4 = Math.floor(Math.random() * (12-1+1)) + 1;
-                console.log(crystal4); 
-                winning = false;
-                losing = false;
-                $("#myScore").html("<p>Your current score is: " + 0 + "</p>");
-                $("#wins").html("<p>Wins: " + wins + "</p>");
-                $("#losses").html("<p>Losses: " + losses + "</p>");
-                $("#message").empty();
-            });
-            
-    }
-})
+            }
+            else if (randomNumber < addingUpNumber) {
+                losing = true;
+                $("#message").append("<p>You Lost...</p>");
+                losses++;
+                $("#losses").html("Losses: " + losses);
 
-});
+            }
+        })
+
+        $("#restartButton").click(function () {
+            randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+            addingUpNumber = 0;
+            crystal1 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+            console.log(crystal1);
+            crystal2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+            console.log(crystal2);
+            crystal3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+            console.log(crystal3);
+            crystal4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+            console.log(crystal4);
+            winning = false;
+            losing = false;
+            $("#myScore").html("<p>Your current score is: " + 0 + "</p>");
+            $("#wins").html("<p>Wins: " + wins + "</p>");
+            $("#losses").html("<p>Losses: " + losses + "</p>");
+            $("#message").empty();
+        });
+
+    })
